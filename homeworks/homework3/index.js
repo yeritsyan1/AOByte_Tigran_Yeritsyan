371d40b40dc3348d3196e7ffb28f128e3d72e2ca
yeritsyan1 <tyeritsyan1@gmail.com>
2023-07-06 12:13:46 +0400

    added for loop

--- homeworks/homework3/index.js
+++ homeworks/homework3/index.js
@@ -2,13 +2,17 @@
         // create parent
         this.parent = document.createElement(type)
         document.body.appendChild(parent)
-        parent.setAttribute('class', attr.class)
+        for(let item in attr ) {
+           this.parent.setAttribute(item, attr[item])
+        }
         parent.innerHTML = value
         const arr = []
         // create child
          const childFunc = (type, attr, value, myChildN) => {
              child = document.createElement(type)
-             child.setAttribute('class', attr.class)
+             for(let item in attr) {
+                child.setAttribute(item, attr[item])
+             }
              child.innerHTML = value
              if(arr.length >= 1) {
                  arr[0]?.['parent'].appendChild(child)
@@ -20,13 +24,13 @@
             arr[0]?.children < 1 && arr.shift()
         }
 
-       childrenArr.forEach((item, index) => {
+       childrenArr.forEach((item) => {
         childFunc(item.type, item.attr, item.value, item.myChildN)
        })
        console.log(this.parent)
     }
 
-       el('div', {class: 'divElement'}, null,
+       el('div', {class: 'divElement', id: 'idDivElement'}, null,
         [
             {type: 'p', attr: {class: 'pElement'}, value: ' First /', myChildN: 0},
             {type: 'p', attr: {class: 'pElement'}, value: ' Second /', myChildN: 0},
@@ -37,5 +41,6 @@
             {type: 'div', attr: {class: 'divElement'}, value: ' Seventh /', myChildN: 0},
             {type: 'span', attr: {class: 'spanElement'}, value: ' Eighth /', myChildN: 0},
             {type: 'span', attr: {class: 'spanElement'}, value: ' Ninth /', myChildN: 0},
+            {type: 'img', attr: {class: 'imgnElement', id: 'McLaren', src: 'https://www.detroitnews.com/gcdn/presto/2022/02/18/PDTN/bf6d85ae-1c56-411c-8214-d14f5448cf9c--1x-1_-_2022-02-18T165358.518.jpg?width=660&height=441&fit=crop&format=pjpg&auto=webp'}, myChildN: 0},
         ]
     )
